a1ad2c3409c204c6507a0702e7e9730eee3d0910
Justin Icenhour <creative515.justin@gmail.com>
2024-10-31 10:11:12 -0500

    configure tor for independent cache.

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,7 @@ use tor_rtcompat::PreferredRuntime;
 use tracing::{event, span, Level};
 use uri::parse_uri;
 use uri::Uri;
+use uuid::Uuid;
 
 mod error;
 mod make_request;
@@ -57,8 +58,11 @@ mod streams;
 mod tor_client;
 mod uri;
 
+static INSTANCE_ID: LazyLock<Uuid> = LazyLock::new(|| { Uuid::new_v4() }); // Initialize TOR_CONFIG here
+
 static TOR_CONFIG: LazyLock<TorClientConfig> = LazyLock::new(|| {
-	let mut default_config = TorClientConfigBuilder::default();
+        let mut default_config = TorClientConfigBuilder::from_directories("./tor/state/".to_owned() + &INSTANCE_ID.to_string(), "./tor/cache/".to_owned() + &INSTANCE_ID.to_string());
+	//let mut default_config = TorClientConfigBuilder::default();
 	default_config.address_filter().allow_onion_addrs(true);
 	default_config.build().unwrap()
 });
